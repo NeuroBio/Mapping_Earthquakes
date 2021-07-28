13409372eb966daeb41536f34412958f5d369c27
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-07-28 16:28:01 -0400

    13.5.2

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,50 @@
-var map = L.map('mapid').setView([40.7, -94.5], 4);
+// Add GeoJSON data.
+const sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
+const map = L.map('mapid').setView([37.5, -122.5], 10);
 
-streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: apiKey
-})
+}).addTo(map);
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: (feature, latlng) => {
+      console.log(feature);
+      return L.marker(latlng)
+        .bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+
+  }).addTo(map);
+
+L.geoJson(sanFranAirport, {
+    onEachFeature: (feature, layer) => {
+        console.log(layer);
+      layer.bindPopup();
+     }
+});
